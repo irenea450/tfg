3470daf951d98fe20dd8dbded43ab0baffb81129
irenea450 <irenedelalamo.alumno@gmail.com>
2025-04-14 12:54:37 +0200

    Cmbio estructura de carpetas de views, cambio el header del trabajador para que se ajuste a mi aplicación

--- routes/trabajador.js
+++ routes/trabajador.js
@@ -2,16 +2,35 @@ const express = require('express');
 const { session } = require('passport');
 const router = express.Router();
 
+//Rutas desde aqui empiezan con --> /zona/trabajador
+
+
 // GET horario
 // Ruta para el horario del trabajador
 router.get('/horario', (req, res) => {
-    res.render('horario', {title: 'Didadent', name: req.session.name});
+    res.render('trabajadores/horario', {title: 'Didadent', name: req.session.name});
 });
 
 // POST horario
 router.post('/horario', async (req, res) => {
 
-    res.render('horario', {
+    res.render('trabajadores/horario', {
+        title: 'Didadent',
+        login: true,
+        name: req.session.name
+    })
+
+});
+
+// GET buscador pacientes
+// Ruta para el buscador de pacientes
+router.get('/pacientes', (req, res) => {
+    res.render('trabajadores/buscadorPacientes', {title: 'Didadent', name: req.session.name});
+});
+// POST buscador pacientes
+router.post('trabajadores/buscadorPacientes', async (req, res) => {
+
+    res.render('buscadorPacientes', {
         title: 'Didadent',
         login: true,
         name: req.session.name
